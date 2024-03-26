6f05122d4a6ac1e9ac1da09797dcdbe69b9e5a9f
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-26 18:04:48 -0300

    Lista de peliculas modularizada

--- Lab1/main.py
+++ Lab1/main.py
@@ -2,22 +2,10 @@ import random
 from flask import Flask, jsonify, request
 from proximo_feriado import NextHoliday
 from unidecode import unidecode
+from pelis import peliculas
 
 app = Flask(__name__)
-peliculas = [
-    {'id': 1, 'titulo': 'Indiana Jones', 'genero': 'Acción'},
-    {'id': 2, 'titulo': 'Star Wars', 'genero': 'Acción'},
-    {'id': 3, 'titulo': 'Interstellar', 'genero': 'Ciencia ficción'},
-    {'id': 4, 'titulo': 'Jurassic Park', 'genero': 'Aventura'},
-    {'id': 5, 'titulo': 'The Avengers', 'genero': 'Acción'},
-    {'id': 6, 'titulo': 'Back to the Future', 'genero': 'Ciencia ficción'},
-    {'id': 7, 'titulo': 'The Lord of the Rings', 'genero': 'Fantasía'},
-    {'id': 8, 'titulo': 'The Dark Knight', 'genero': 'Acción'},
-    {'id': 9, 'titulo': 'Inception', 'genero': 'Ciencia ficción'},
-    {'id': 10, 'titulo': 'The Shawshank Redemption', 'genero': 'Drama'},
-    {'id': 11, 'titulo': 'Pulp Fiction', 'genero': 'Crimen'},
-    {'id': 12, 'titulo': 'Fight Club', 'genero': 'Drama'}
-]
+
 
 
 def obtener_peliculas():
