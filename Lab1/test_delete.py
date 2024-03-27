f5b990723f32d30a0458748a305573e083a9ec4c
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-27 14:04:34 -0300

    Mejora de estilos en test_delete

--- Lab1/test_delete.py
+++ Lab1/test_delete.py
@@ -2,16 +2,16 @@ import requests
 
 def delete_test():
   print()
-  print("----------------------------------------")
+  print("========================================")
   print("Inicio de las pruebas del método DELETE.")
-  print("----------------------------------------")
+  print("========================================")
 
   # Eliminar una película
   ## Respuesta correcta (200)
   print("------------------------------------------------")
   print("Eliminar una película. Respuesta correcta [200].")
   print("------------------------------------------------")
-  id_peliculas = [1, 2, 3]
+  id_peliculas = [1, 2, 3, 4]
   for id_pelicula in id_peliculas:
     response = requests.delete(f'http://localhost:5000/peliculas/{id_pelicula}')
     if response.status_code == 200:
@@ -21,7 +21,6 @@ def delete_test():
     print()
 
   ## Respuesta incorrecta (404)
-  print()
   print("--------------------------------------------------")
   print("Eliminar una película. Respuesta incorrecta [404].")
   print("--------------------------------------------------")
