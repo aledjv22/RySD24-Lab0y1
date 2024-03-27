542a0122f74b04555a1ca647d16eb0da95989f23
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-27 14:28:58 -0300

    Mejora de respuestas en test_post

--- Lab1/test_post.py
+++ Lab1/test_post.py
@@ -2,9 +2,9 @@ import requests
 
 def post_test():
   print()
-  print("--------------------------------------")
+  print("======================================")
   print("Inicio de las pruebas del método POST.")
-  print("--------------------------------------")
+  print("======================================")
 
   # Agregar una nueva película
   ## Respuesta correcta (201)
@@ -27,7 +27,6 @@ def post_test():
     print()
   
   ## Respuesta incorrecta (400)
-  print()
   print("-------------------------------------------------------")
   print("Agregar una nueva película. Respuesta incorrecta [400].")
   print("-------------------------------------------------------")
@@ -39,10 +38,12 @@ def post_test():
   for pelicula in peliculas:
     response = requests.post('http://localhost:5000/peliculas', json=pelicula)
     if response.status_code == 400:
-      print(f"Error captado de forma correcta, falta de datos de la película o datos incorrectos.")
-    else:
-      print(f"ERROR, si se agrego la pelicula, se obtuvo {response.status_code} en lugar de 400.")
+      print("Error captado de forma correcta, falta de datos de la película.")
+    elif response.status_code == 201:
+      print(f"ERROR GRAVE, se agrego la pelicula y se obtuvo {response.status_code} en lugar de 400.")
       pelicula_agregada = response.json()
       print("Película agregada:")
       print(f"Título: {pelicula_agregada['titulo']}, Género: {pelicula_agregada['genero']}")
+    else:
+      print(f"ERROR, se obtuvo {response.status_code} en lugar de 400.")
     print()
